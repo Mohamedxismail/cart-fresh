0ab9ed2184ddd5ba2fe2c0967347d72e824a3c3c
Mohamedxismail <qolkilam@gmail.com>
2026-08-02 06:34:48 +0300

    fix  ui ux

--- src/components/LayOut/LayOut.jsx
+++ src/components/LayOut/LayOut.jsx
@@ -3,10 +3,12 @@ import styles from './LayOut.module.css'
 import { Outlet } from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
+import ScrollToTop from '../Home/Component/ScrollToTop/ScrollToTop'
 export default function LayOut() {
   const [count, setCount] = useState(0)
   return (
     <div>
+      <ScrollToTop/>
       <NavBar />
 
 
